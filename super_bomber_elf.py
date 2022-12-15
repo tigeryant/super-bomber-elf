e019f5cd0f394e78605369398fdbeebc176b8dfd
John Davies <jd.other@hotmail.com>
2022-12-15 20:38:12 +0000

    final solution (WIP)

--- super_bomber_elf.py
+++ super_bomber_elf.py
@@ -1,16 +1,34 @@
 import asyncio
 import json
+import getch
 # import future
 from websockets import connect
+
+
+def play(arg):
+  validCommands = [{"l" : {"command":"Look"}}, {"r" : {"command":"DropBomb"}},  {"na" : {"command":["SetName","<name>"]}}
+  ,"n" : {"command":"MoveNorth"}, "s" :{"command":"MoveSouth"}, "e" : {"command":"MoveEast"}, "a" : {"command":"MoveWest"}] 
+  return validCommands[arg]
 
 async def hello(uri):
     async with connect(uri) as websocket:
         # await websocket.send("Hello world!")
         response = await websocket.recv()
         print(response)
+        await websocket.send(json.dumps({"command":["SetName","PowerRangers"]}))
+        
+        while(True):
+          command = getch.getch()
 
-        # await websocket.send(json.dumps({"command":["SetName","John and Naomi"]}))
-        await websocket.send(json.dumps({"command":"MoveNorth"}))
-        await asyncio.Future()
+          await websocket.send(json.dumps(play(command)))
+          await asyncio.Future()
+
+        # commansd we pass na
+
+        
+        # await websocket.send(json.dumps({"command":"MoveEast"}))
+        # await websocket.send(json.dumps({"command":"MoveWest"}))
+        # await websocket.send(json.dumps({"command":"MoveSouth"}))
+        # await websocket.send(json.dumps({"command":"Look"}))
 
 asyncio.run(hello("ws://172.16.173.86:8080/"))
